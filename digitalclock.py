51111cec740addad9cb959bf7648353ae12877da
markalirjes <142129439+markalirjes@users.noreply.github.com>
2023-08-12 18:12:21 -0800

    Add files via upload
    they are my simple project like calculator and digitalclock and guess the password is a genetic engine for solve this problem(l like this)

---
+++ digitalclock.py
@@ -0,0 +1,21 @@
+from tkinter import Tk
+from tkinter import Label
+import time
+import sys
+
+master = Tk()
+
+master.title("digitalclock")
+
+def get_time():
+    timeVar = time.strftime("%I:%M:%S %p")
+    clock.config(text = timeVar)
+    clock.after(200, get_time)
+
+
+clock = Label(master, font=("Arial", 100),bg = "black" , fg="green")
+clock.pack()
+
+get_time()
+
+master.mainloop()
