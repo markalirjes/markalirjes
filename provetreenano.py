e347a0d79df4578a33c8b2a0244ab7c0f3057364
mark_alirjes <142129439+markalirjes@users.noreply.github.com>
2023-11-09 07:05:43 -0800

    Add files via upload

---
+++ provetreenano.py
@@ -0,0 +1,17 @@
+#Well, let's go to prove it
+p = 2
+#price is const in all algoritm
+benefitrate = supply * p
+#productleft is same for scale time
+Diversionrate = productleft / 100
+#qc is fixed cost and same for all year 
+costrate = Diversionrate + qc
+#nanoproduct is a Several products that are collected in one product
+nanorate = 1 / nanoproduct
+#In a developed economy
+y = benefitrate - costrate 
+y = supply * p - Diversionrate + qc
+#in nano scale economy
+y1 = benefitrate1 - costrate1
+y1 = nanorate * supply * p - 0 + qc
+y1 > y 
