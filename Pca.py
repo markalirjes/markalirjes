62fa15c1a03a70b7629c3426c4bc4a131bbb285a
mark_alirjes <142129439+markalirjes@users.noreply.github.com>
2023-12-01 05:24:48 -0800

    PCA ALGORITM with sklearn library in python
    pca algorithm with sklearn

---
+++ Pca.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+
+cancer.keys()
+print(cancer['DECR'])
+
+df = pd.DataFrame(cancer['data'] , columns=cancer['feature_names'])
+
+df.head(5)
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+scaler.fit(df)
+
+scaled_data = scaler.transform(df)
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+pca.fit(scaled_data)
+x_pca = pca.transform(scaled_data)
+
+plt.figure(figsize=(8,6))
+plt.scatter(x_pca[:,0],x_pca[:,1],c=cancer['target'])
+plt.xlabel("First principle component")
+plt.ylabel("Second principle component")
