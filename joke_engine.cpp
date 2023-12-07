5c495a4abc5928b02fddce4424a16445865f3a7a
mark_alirjes <142129439+markalirjes@users.noreply.github.com>
2023-12-07 01:34:25 -0800

    explain joke engine in llms
    explain joke engine in llms

---
+++ joke_engine.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//hello , it's a bug in llms bot, they cant make jokes I try to explain solve this problem in this program
+// first , llm give sentences about joke and delete main words.
+
+int main() {
+    string llmsentence;
+    llmsentence = "its many words";
+    string mainwords;
+    mainwords = "give a word matter in sentence and delete a part of this these useing indipendent";
+    std::cout << llmsentence << mainwords <<std::endl;
+    return 0;
+}
